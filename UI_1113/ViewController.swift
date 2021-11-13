2f17a046f2bae2c06eee20f23154d63f4f47aaa2
weiwi <marcoer@gmail.com>
2021-11-13 13:56:35 +0800

    Initial Commit

---
+++ UI_1113/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI_1113
+//
+//  Created by WEI-YI Wang on 2021/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
